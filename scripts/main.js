6b24ae965bd47b1e85cf58091d294c807becd119
Vladislav Tochaev <vlad.tochaev2021@gmail.com>
2025-07-30 19:25:37 +0400

    feat: release

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import TabsCollection from './Tabs.js'
 import VideoPlayerCollection from './VideoPlayer.js'
 import ExpandableContentCollection from './ExpandableContent.js';
 import InputMaskCollection from './InputMask.js'
-import SelectCollection from './Select'
+import SelectCollection from './Select.js'
 
 new Header()
 new TabsCollection()
